1f61934f8caf22bc8411ab50e7a2bcc25ce12b71
Andre de Camargo <camargo_and@hotmail.com>
2021-10-20 17:03:12 -0300

    Parte 11 - Teste unitario

---
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Andre";
+    private static final String LAST_NAME = "Camargo";
+    private static final String CPF_NUMBER = "341.784.368-56";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(1985, 3, 8);
+
+    public static PersonDTO createFakeDTO(){
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("08-03-1985")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity(){
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+
+    }
+}
